2fc9b37bcbe7a71c5927902b47c5c41a9bf78ee1
Patrick <patrick.rodgers@microsoft.com>
2016-05-13 13:54:36 -0400

    added comments to queryable methods, updated orderBy, finalized updates to package

--- tests/sharepoint/rest/queryable.test.ts
+++ tests/sharepoint/rest/queryable.test.ts
@@ -1,25 +1,87 @@
-// "use strict";
+"use strict";
 
-// import { expect } from "chai";
-// import { Queryable } from "./Queryable";
+import { expect } from "chai";
+import { QueryableInstance, QueryableCollection } from "../../../src/sharepoint/rest/Queryable";
 
-// describe("Queryable", () => {
-//     it("Should be an object", () => {
-//         let queryable = new Queryable([], "");
-//         expect(queryable).to.be.a("object");
-//     });
-//     // describe("select", () => {
-//     //     it("Should append a select query", () => {
-//     //         let queryable = new Queryable(["_api/web"], "/lists");
-//     //         queryable.select(["Title", "Created"]);
-//     //         expect(queryable.url()).to.include("$select=Title,Created");
-//     //     });
-//     // });
-//     // describe("filter", () => {
-//     //     it("Should append a filter query", () => {
-//     //         let queryable = new Queryable(["_api/web"], "/lists");
-//     //         queryable.filter("Title eq 'Tasks'");
-//     //         expect(queryable.url()).to.include("$filter=Title eq 'Tasks'");
-//     //     });
-//     // });
-// });
+describe("QueryableInstance", () => {
+
+    let queryable: QueryableInstance;
+
+    beforeEach(() => {
+        queryable = new QueryableInstance("_api/web");
+    });
+
+    describe("select", () => {
+        it("Should append a select query", () => {
+            queryable.select("Title", "Description");
+            expect(queryable.toUrlAndQuery()).to.include("$select=Title,Description");
+        });
+    });
+
+    describe("expand", () => {
+        it("Should append an expand query", () => {
+            queryable.expand("User\\Email", "Another\\Field");
+            expect(queryable.toUrlAndQuery()).to.include("$expand=User\\Email,Another\\Field");
+        });
+    });
+});
+
+describe("QueryableCollection", () => {
+
+    let queryable: QueryableCollection;
+
+    beforeEach(() => {
+        queryable = new QueryableCollection("_api/web");
+    });
+
+    describe("expand", () => {
+        it("Should append an expand query", () => {
+            queryable.expand("User\\Email", "Another\\Field");
+            expect(queryable.toUrlAndQuery()).to.include("$expand=User\\Email,Another\\Field");
+        });
+    });
+
+    describe("filter", () => {
+        it("Should append a filter query", () => {
+            queryable.filter("Title eq 'Tasks'");
+            expect(queryable.toUrlAndQuery()).to.include("$filter=Title eq 'Tasks'");
+        });
+    });
+
+    describe("orderBy", () => {
+        it("Should append a single order by query ascending", () => {
+            queryable.orderBy("Title", true);
+            expect(queryable.toUrlAndQuery()).to.include("$orderby=Title asc");
+        });
+    });
+
+    describe("orderBy", () => {
+        it("Should append multiple order by queries", () => {
+            queryable.orderBy("Title", true);
+            queryable.orderBy("Description");
+            expect(queryable.toUrlAndQuery()).to.include("$orderby=Title asc,Description");
+        });
+    });
+
+    describe("select", () => {
+        it("Should append a select query", () => {
+            queryable.select("Title", "Description");
+            expect(queryable.toUrlAndQuery()).to.include("$select=Title,Description");
+        });
+    });
+
+    describe("skip", () => {
+        it("Should append a skip query", () => {
+            queryable.skip(10);
+            expect(queryable.toUrlAndQuery()).to.include("$skip=10");
+        });
+    });
+
+    describe("top", () => {
+        it("Should append a top query", () => {
+            queryable.top(5);
+            expect(queryable.toUrlAndQuery()).to.include("$top=5");
+        });
+    });
+});
+
