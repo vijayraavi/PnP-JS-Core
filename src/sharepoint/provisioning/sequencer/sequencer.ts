dd052a609450beb81f3116ed7f083dd2edc39984
Patrick <patrick.rodgers@microsoft.com>
2016-05-24 11:20:55 -0400

    Fixing submitted code so it passes linting and builds. Also added better typings and updated use of 'this'

--- src/sharepoint/provisioning/sequencer/sequencer.ts
+++ src/sharepoint/provisioning/sequencer/sequencer.ts
@@ -5,24 +5,31 @@ export class Sequencer {
     /**
      * Creates a new instance of the Sequencer class, and declare private variables
      */
-    constructor(private functions:Array<any>, private parameter:any, private scope:any) {
+    constructor(private functions: Array<any>, private parameter: any, private scope: any) {
     }
 
     /**
      * Executes the functions in sequence using DeferredObject
      */
-    public execute(progressFunction:any) {
-        const thisSequencer = this;//a meaningful name for the this scope
-        var promiseSequence = Promise.resolve();// empty promise to chain on
-        thisSequencer.functions.forEach( (sequenceFunction, functionNr) =>{
-            promiseSequence = promiseSequence.then(function () {// Chain function call onto the sequence
-                //console.info('executing function:',functionNr);
-                return sequenceFunction.call(thisSequencer.scope, thisSequencer.parameter);
-            }).then(function (result) {// Resolve for each function call
-                if (progressFunction) progressFunction.call(thisSequencer, functionNr, thisSequencer.functions);
-                //console.info('finished function:',functionNr,'of',thisSequencer.functions);
+    public execute(progressFunction?: (s: Sequencer, index: number, functions: any[]) => void): Promise<void> {
+
+        let promiseSequence = Promise.resolve<void>(); // empty promise to chain on
+
+        this.functions.forEach((sequenceFunction, functionNr) => {
+
+            promiseSequence = promiseSequence.then(function () { // Chain function call onto the sequence
+
+                return sequenceFunction.call(this.scope, this.parameter);
+
+            }).then(function (result) { // Resolve for each function call
+
+                if (progressFunction) {
+                    progressFunction.call(this, functionNr, this.functions);
+                }
             });
-        });
-        return promiseSequence;// This will resolve after the entire chain is resolved
+
+        }, this);
+
+        return promiseSequence; // This will resolve after the entire chain is resolved
     }
 }
