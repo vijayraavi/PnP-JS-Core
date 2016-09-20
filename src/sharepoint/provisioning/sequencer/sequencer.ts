24237c5ffb08dbf9d092bb155a0f8f12671d4bff
Patrick <patrick.rodgers@microsoft.com>
2016-09-20 10:55:35 -0400

    fix for 196

--- src/sharepoint/provisioning/sequencer/sequencer.ts
+++ src/sharepoint/provisioning/sequencer/sequencer.ts
@@ -19,11 +19,11 @@ export class Sequencer {
 
         this.functions.forEach((sequenceFunction, functionNr) => {
 
-            promiseSequence = promiseSequence.then(function () { // Chain function call onto the sequence
+            promiseSequence = promiseSequence.then(() => { // Chain function call onto the sequence
 
                 return sequenceFunction.call(this.scope, this.parameter);
 
-            }).then(function (result) { // Resolve for each function call
+            }).then((result) => { // Resolve for each function call
 
                 if (progressFunction) {
                     progressFunction.call(this, functionNr, this.functions);
