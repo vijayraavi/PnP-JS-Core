d4172d4ff9eb387df1754eb88bd3eb4fcc13fd2e
Patrick Rodgers <patrodg@microsoft.com>
2017-02-13 20:03:13 -0500

    version 0.6 of .travis.yml

--- gulptasks/test.js
+++ gulptasks/test.js
@@ -21,13 +21,14 @@ gulp.task("_istanbul:hook", ["build:testing"], () => {
         .pipe(istanbul.hookRequire());
 });
 
-gulp.task("test:travis", ["clean", "build:testing", "_istanbul:hook"], () => {
+gulp.task("test:travis", ["clean", "build:testing"], () => {
 
-    console.log("PnPTesting_ClientId: " + process.env.PnPTesting_ClientId);
-    console.log("PnPTesting_ClientSecret: " + process.env.PnPTesting_ClientSecret);
-    console.log("PnPTesting_SiteUrl: " + process.env.PnPTesting_SiteUrl);
-    console.log("PnPTesting_NotificationUrl: " + process.env.PnPTesting_NotificationUrl);
+    // console.log("PnPTesting_ClientId: " + process.env.PnPTesting_ClientId);
+    // console.log("PnPTesting_ClientSecret: " + process.env.PnPTesting_ClientSecret);
+    // console.log("PnPTesting_SiteUrl: " + process.env.PnPTesting_SiteUrl);
+    // console.log("PnPTesting_NotificationUrl: " + process.env.PnPTesting_NotificationUrl);
 
+    // we shim up the global settings here from the environment vars configured for travis
     global.settings = {
         testing: {
             clientId: "",
